66cca247ce9489b2d9fdf0f8d0c716f3dddd54ea
Strkss <88977475+Strkss@users.noreply.github.com>
2021-11-03 15:47:07 +0700

    Easier for newbies

--- CPP_TEMPLATE.cpp
+++ CPP_TEMPLATE.cpp
@@ -1,43 +1,46 @@
 //Thư viện
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+//#include <iomanip>
+//#include <string>
+//#include <algorithm>
+//#include <cmath>
 
 //Namespace
 using namespace std;
 
+//Function
+
 //Define
-#define Text "Hello World;"
-#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define wh while
-#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)
-#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
-#define in cin>>
-#define out cout<<
-#define ll long long
-#define lo long
-#define dou double
-#define ld long double
-#define fl float
-#define sum(a,b) a+b
-#define sub(a,b) a-b
-#define mul(a,b) a*b
-#define div(a,b) a/b
-#define PI 3.14159265358979323846
-#define PIn 3.14
-#define en <<"\n"
-#define fin(wt) freopen("wt.inp","r",stdin)
-#define fout(wt) freopen("wt.out","w",stdout)
-#define uc(a,b) __gcd(a,b)
-#define bc(a,b) (a*b)/(__gcd(a,b))
-#define sortup(a,n) sort(a+1,a+n+1)
-#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())
-#define AmuB pow(A,B)
-#define elif else if
-#define ndmax 1976807737
-#define rt return
-#define HelloWord "Hello, Im Strkss!"
-#define outfi(o,n) cout<<fixed<<setprecision(o)<<n
-#define upper(a,i,s) upper_bound(a,a+1+i,s)
-#define lower(a,i,s) lower_bound(a,a+1+i,s)
+#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);     //Fast Input/Output
+#define wh while                                                        //while
+#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)                               //For Up    (Vòng lặp tiến)
+#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)                               //For Down  (Vòng lặp lùi)
+#define in cin>>                                                        //cin
+#define out cout<<                                                      //cout
+#define ll long long                                                    //long long
+#define lo long                                                         //long
+#define dou double                                                      //double
+#define ld long double                                                  //long double
+#define fl float                                                        //float
+#define sum(a,b) a+b                                                    //Sum       (Tổng)
+#define sub(a,b) a-b                                                    //Sub       (Hiệu)
+#define mul(a,b) a*b                                                    //Mul       (Tích)
+#define div(a,b) a/b                                                    //Div       (Thương)
+#define PI 3.14159265358979323846                                       //PI
+#define PIn 3.14                                                        //PI        (Rounded)
+#define en <<"\n"                                                       //Endl      (Xuống dòng)
+#define fin(wt) freopen("wt.inp","r",stdin)                             //File Input    (Đọc file)
+#define fout(wt) freopen("wt.out","w",stdout)                           //File Output   (Xuất file)
+#define uc(a,b) __gcd(a,b)                                              //GCD       (Ước)
+#define bc(a,b) (a*b)/(__gcd(a,b))                                      //LCM       (Bội)
+#define sortup(a,n) sort(a+1,a+n+1)                                     //Sort Up   (SX Tăng)
+#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())                   //Sort Down (SX Giảm)
+#define AmuB pow(A,B)                                                   //Power     (Lũy thừa)
+#define elif else if                                                    //else if
+#define rt return                                                       //return
+#define outfi(o,n) cout<<fixed<<setprecision(o)<<n                      //SetPrecision  (Độ chính xác)
+#define upper(a,i,s) upper_bound(a,a+1+i,s)                             //Upper Bound   (GT lớn hơn)
+#define lower(a,i,s) lower_bound(a,a+1+i,s)                             //Lower Bound   (GT nhỏ hơn)
 #define getl(s) getline(cin,s);
 
 //Main
