0118f2c01fa53af80f2a80408af2948c01753614
Strkss <88977475+Strkss@users.noreply.github.com>
2021-11-03 10:24:55 +0700

    Create CPP_TEMPLATE.cpp

---
+++ CPP_TEMPLATE.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define Text HelloWord
+#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define wh while
+#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)
+#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define in cin>>
+#define out cout<<
+#define ll long long
+#define lo long
+#define dou double
+#define ld long double
+#define fl float
+#define sum(a,b) a+b
+#define sub(a,b) a-b
+#define mul(a,b) a*b
+#define div(a,b) a/b
+#define PI 3.14159265358979323846
+#define PIn 3.14
+#define en <<"\n"
+#define fin(wt) freopen("wt.inp","r",stdin)
+#define fout(wt) freopen("wt.out","w",stdout)
+#define uc(a,b) __gcd(a,b)
+#define bc(a,b) (a*b)/(__gcd(a,b))
+#define sortup(a,n) sort(a+1,a+n+1)
+#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())
+#define AmuB pow(A,B)
+#define elif else if
+#define ndmax 1976807737
+#define rt return
+#define HelloWord "Hello I'm Apok this's my acc and my code , do not copy it"
+#define outfi(o,n) cout<<fixed<<setprecision(o)<<n
+#define upper(a,i,s) upper_bound(a,a+1+i,s)
+#define lower(a,i,s) lower_bound(a,a+1+i,s)
+#define getl(s) getline(cin,s);
+
+int main()
+{
+    Fast_IO;
+    
+    return 0;
+}
