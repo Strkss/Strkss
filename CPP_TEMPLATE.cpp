00b673d0bef0f310c5b1db513386d995a82ecc8d
Strkss <88977475+Strkss@users.noreply.github.com>
2021-11-07 14:34:09 +0700

    Better Simpler

--- CPP_TEMPLATE.cpp
+++ CPP_TEMPLATE.cpp
@@ -1,45 +1,26 @@
-//Thư viện
 #include <bits/stdc++.h>
-
-//Namespace
 using namespace std;
 
-//Function
+#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);    
+#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)                               
+#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)                              
+#define in cin>>                                                        
+#define out cout<<                                                      
+#define ll long long                                                    
+#define lo long                                                         
+#define dou double                                                      
+#define ld long double                                                  
+#define fl float                                                       
+#define PI 3.14159265358979323846                                      
+#define en <<"\n"                                                       
+#define uc(a,b) gcd(a,b)                                              
+#define bc(a,b) lcm(a,b)                                      
+#define sortup(a,n) sort(a+1,a+n+1)                                     
+#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())                   
+#define elif else if                                                    
+#define rt return                                                       
+#define outfi(o,n) cout<<fixed<<setprecision(o)<<n                      
 
-//Define
-#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);     //Fast Input/Output
-#define wh while                                                        //while
-#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)                               //For Up    (Vòng lặp tiến)
-#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)                               //For Down  (Vòng lặp lùi)
-#define in cin>>                                                        //cin
-#define out cout<<                                                      //cout
-#define ll long long                                                    //long long
-#define lo long                                                         //long
-#define dou double                                                      //double
-#define ld long double                                                  //long double
-#define fl float                                                        //float
-#define sum(a,b) a+b                                                    //Sum       (Tổng)
-#define sub(a,b) a-b                                                    //Sub       (Hiệu)
-#define mul(a,b) a*b                                                    //Mul       (Tích)
-#define div(a,b) a/b                                                    //Div       (Thương)
-#define PI 3.14159265358979323846                                       //PI
-#define PIn 3.14                                                        //PI        (Rounded)
-#define en <<"\n"                                                       //Endl      (Xuống dòng)
-#define fin(wt) freopen("wt.inp","r",stdin)                             //File Input    (Đọc file)
-#define fout(wt) freopen("wt.out","w",stdout)                           //File Output   (Xuất file)
-#define uc(a,b) __gcd(a,b)                                              //GCD       (Ước)
-#define bc(a,b) (a*b)/(__gcd(a,b))                                      //LCM       (Bội)
-#define sortup(a,n) sort(a+1,a+n+1)                                     //Sort Up   (SX Tăng)
-#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())                   //Sort Down (SX Giảm)
-#define AmuB pow(A,B)                                                   //Power     (Lũy thừa)
-#define elif else if                                                    //else if
-#define rt return                                                       //return
-#define outfi(o,n) cout<<fixed<<setprecision(o)<<n                      //SetPrecision  (Độ chính xác)
-#define upper(a,i,s) upper_bound(a,a+1+i,s)                             //Upper Bound   (GT lớn hơn)
-#define lower(a,i,s) lower_bound(a,a+1+i,s)                             //Lower Bound   (GT nhỏ hơn)
-#define getl(s) getline(cin,s);
-
-//Main
 int main()
 {
     Fast_IO;
