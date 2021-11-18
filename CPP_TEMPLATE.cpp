08cfc85c64ce489e12393d89286911ec2c0209c2
Strkss <88977475+Strkss@users.noreply.github.com>
2021-11-18 19:14:47 +0700

    Template

--- CPP_TEMPLATE.cpp
+++ CPP_TEMPLATE.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);    
-#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)                               
-#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)                              
-#define in cin>>                                                        
-#define out cout<<                                                      
-#define ll long long                                                    
-#define lo long                                                         
-#define dou double                                                      
-#define ld long double                                                  
-#define fl float                                                       
-#define PI 3.14159265358979323846                                      
-#define en <<"\n"                                                       
-#define uc(a,b) gcd(a,b)                                              
-#define bc(a,b) lcm(a,b)                                      
-#define sortup(a,n) sort(a+1,a+n+1)                                     
-#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())                   
-#define elif else if                                                    
-#define rt return                                                       
-#define outfi(o,n) cout<<fixed<<setprecision(o)<<n                      
+#define Fast_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fu(i,a,b,c) for(ll i=a;i<=b;i+=c)
+#define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define in cin>>
+#define out cout<<
+#define ll long long
+#define lo long
+#define dou double
+#define ld long double
+#define fl float
+#define PI 3.14159265358979323846
+#define en <<"\n"
+#define uc(a,b) gcd(a,b)
+#define bc(a,b) lcm(a,b)
+#define sortup(a,n) sort(a+1,a+n+1)
+#define sortdw(a,t,n) sort(a+1+t,a+1+n,greater<ll>())
+#define elif else if
+#define rt return
+#define outfi(o,n) cout<<fixed<<setprecision(o)<<n
 
 int main()
 {
